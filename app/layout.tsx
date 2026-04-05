e30db876fcff769833a759ee65eeaf7d2fc6dd1c
Damini Deshmukh <daminideshmukh@Daminis-MacBook-Air.local>
2026-04-05 16:22:30 +0530

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono ,Pixelify_Sans,Inter } from "next/font/google";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -9,6 +10,16 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const GameFont= Pixelify_Sans({
+  variable: "--font-game",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -25,9 +36,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${GameFont.variable} ${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Welcome to the home page! This is where you can find all the latest updates and news about our application. Stay tuned for more exciting features and improvements coming your way soon!
+      <h1 className="font-game">Welcome to the home page!</h1> 
+    <Button>Subscribe</Button>
+    
     </div>
   );
 }
