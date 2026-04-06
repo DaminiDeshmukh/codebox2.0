e1b7e30eeb130350156104848b054747e33c971c
Damini Deshmukh <daminideshmukh2106@gmail.com>
2026-04-06 18:41:22 +0530

    new changes 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono ,Pixelify_Sans,Inter } from "next/font/google";
+import { Geist, Geist_Mono ,Pixelify_Sans,Inter,Jersey_10 } from "next/font/google";
 import "./globals.css";
+import Provider from "./provider";
 
 
 const geistSans = Geist({
@@ -13,9 +14,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const GameFont= Pixelify_Sans({
+const GameFont= Jersey_10({
   variable: "--font-game",
   subsets: ["latin"],
+  weight:"400"
 });
 
 const inter = Inter({
@@ -36,9 +38,17 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      
-    >
-      <body className={`${geistSans.variable} ${geistMono.variable} ${GameFont.variable} ${inter.variable} antialiased`}>{children}</body>
+      suppressHydrationWarning
+      className="dark">
+      <body className={`${geistSans.variable} ${geistMono.variable} ${GameFont.variable} ${inter.variable} antialiased`}>
+        <Provider  attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange>
+          {children}
+        </Provider>
+        
+        </body>
     </html>
   );
 }
